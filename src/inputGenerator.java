0e79abacc280da184dd2af9152b64c78d0d0fffb
pmchrist <pmchrist@csd.auth.gr>
2019-05-27 22:53:04 +0300

    Remembered input generator

---
+++ src/inputGenerator.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+public class inputGenerator {
+
+    public inputGenerator(int ants) throws IOException {
+        FileWriter fileWriter = new FileWriter(new File("input.txt"));
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        Random random = new Random();
+
+        for (int i = 1; i < ants + 1; i++) {
+            printWriter.print(i + " " + random.nextDouble()*500 + " " + random.nextDouble()*500 + " ");
+            if (i % 2 == 1) printWriter.print(random.nextInt(10000));
+            else {
+                for (int j = 0; j < 5; j++)
+                    printWriter.print(random.nextInt(1000) + " ");
+            }
+            printWriter.println("");
+        }
+        printWriter.flush();
+        printWriter.close();
+        fileWriter.close();
+        System.out.println("File created.");
+    }
+}
